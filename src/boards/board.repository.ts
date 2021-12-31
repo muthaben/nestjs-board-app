105539d6d71fb8d971a21dc97d3880a720fb7570
muthaben <senarkimi@gmail.com>
2021-12-31 14:06:20 +0900

    feat: Create repository for boards

---
+++ src/boards/board.repository.ts
@@ -0,0 +1,6 @@
+import { EntityRepository, Repository } from 'typeorm';
+import { Board } from './board.entity';
+
+@EntityRepository(Board) // class가 Board를 컨트롤 하는 사용자 정의 repository임을 선언
+// extends: entity를 삽입, 삭제 등 컨트롤하기 위해서 필요한 키워드
+export class BoardRepository extends Repository<Board> {}
--- src/boards/boards.module.ts
+++ src/boards/boards.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { BoardRepository } from './board.repository';
 import { BoardsController } from './boards.controller';
 import { BoardsService } from './boards.service';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([BoardRepository])],
   // boards.controller.ts 파일 생성 후 사용할 수 있도록 자동 등록
   controllers: [BoardsController],
   // boards.service.ts 파일 생성 후 자동 등록
