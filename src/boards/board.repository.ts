4897bf2881c55ad39ea6bcd57084bf65f64a6240
muthaben <senarkimi@gmail.com>
2021-12-31 14:06:56 +0900

    feat: Create repository for boards

--- src/boards/board.repository.ts
+++ src/boards/board.repository.ts
@@ -2,5 +2,5 @@ import { EntityRepository, Repository } from 'typeorm';
 import { Board } from './board.entity';
 
 @EntityRepository(Board) // class가 Board를 컨트롤 하는 사용자 정의 repository임을 선언
-// extends: entity를 삽입, 삭제 등 컨트롤하기 위해서 필요한 키워드
+// extends: entity를 삽입, 조회, 수정, 삭제 등 컨트롤하기 위해서 필요한 키워드
 export class BoardRepository extends Repository<Board> {}
