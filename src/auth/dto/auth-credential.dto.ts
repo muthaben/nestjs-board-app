59b03a5f89e84d31af1b901f70c74daf61a0dace
muthaben <senarkimi@gmail.com>
2022-01-02 11:34:12 +0900

    feat: Add validation check on username and password using class-validator

--- src/auth/dto/auth-credential.dto.ts
+++ src/auth/dto/auth-credential.dto.ts
@@ -1,4 +1,17 @@
+import { IsString, Matches, MaxLength, MinLength } from 'class-validator';
+
 export class AuthCredentialDto {
+  @IsString()
+  @MinLength(4)
+  @MaxLength(20)
   username: string;
+
+  @IsString()
+  @MinLength(4)
+  @MaxLength(20)
+  @Matches(/^[a-zA-Z0-9]*$/, {
+    message:
+      'Password con contain only leters and numbers. No special characters are allowed.',
+  })
   password: string;
 }
