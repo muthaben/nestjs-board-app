ccb810b5298531274516d6267e7b38542ece8c9b
muthaben <senarkimi@gmail.com>
2022-01-02 12:14:23 +0900

    feat: Add validation check for unique username

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,3 +1,7 @@
+import {
+  ConflictException,
+  InternalServerErrorException,
+} from '@nestjs/common';
 import { EntityRepository, Repository } from 'typeorm';
 import { AuthCredentialDto } from './dto/auth-credential.dto';
 import { User } from './user.entity';
@@ -7,6 +11,15 @@ export class UserRepository extends Repository<User> {
   async createUser(authCredentialsDto: AuthCredentialDto): Promise<void> {
     const { username, password } = authCredentialsDto;
     const user = this.create({ username, password });
-    await this.save(user);
+    // try catch 처리해주지 않으면 Internal server error가 그대로 contoller 레벨로 이동하여 500 에러를 클라이언트에 보냄
+    try {
+      await this.save(user);
+    } catch (error) {
+      if (error.code === '23505') {
+        throw new ConflictException('Existing username');
+      } else {
+        throw new InternalServerErrorException();
+      }
+    }
   }
 }
