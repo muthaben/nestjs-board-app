5a2adae3fb74b9a7b16c55713299e3086ecae882
muthaben <senarkimi@gmail.com>
2022-01-03 23:06:42 +0900

    Fix typo

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -5,14 +5,14 @@ import {
 import { EntityRepository, Repository } from 'typeorm';
 import { AuthCredentialDto } from './dto/auth-credential.dto';
 import { User } from './user.entity';
-import * as bycrypt from 'bcryptjs';
+import * as bcrypt from 'bcryptjs';
 
 @EntityRepository(User)
 export class UserRepository extends Repository<User> {
   async createUser(authCredentialsDto: AuthCredentialDto): Promise<void> {
     const { username, password } = authCredentialsDto;
-    const salt = await bycrypt.genSalt();
-    const hashedPassword = await bycrypt.hash(password, salt);
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(password, salt);
     const user = this.create({ username, password: hashedPassword });
 
     // try catch 처리해주지 않으면 Internal server error가 그대로 contoller 레벨로 이동하여 500 에러를 클라이언트에 보냄
